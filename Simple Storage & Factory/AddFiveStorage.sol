3f0a6e101d5872d6aaa17eeed3e8582b9dc29c14
Dipak Tapase <dipaktapase1512@gmail.com>
2023-10-12 17:54:05 +0530

    lesson 2 & 3

---
+++ Simple Storage & Factory/AddFiveStorage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {SimpleStorage} from "SimpleStorage.sol";
+
+contract AddFiveStorage is SimpleStorage{
+
+    // function sayHello() public pure returns(string memory) {
+    //     return "Hello";
+    // }
+
+    function store(uint256 _newNumber) public override {
+        myFavoriteNumber = _newNumber + 5;
+    }
+}
---
+++ Simple Storage & Factory/SimpleFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Named Imports are really important to save lot of gas and other things.
+import {SimpleStorage} from "SimpleStorage.sol";
+
+contract StorageFacotry {
+
+    SimpleStorage[] public listOfSimpleStorageContracts;
+    
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        // SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+}
