da68440bfe29a93f092b7b5187cd7febd0e97901
Dipak Tapase <dipaktapase1512@gmail.com>
2023-10-17 11:47:15 +0530

    Foundry simple storage

---
+++ 3. Foundry Simple Storage/src/SimpleStorage.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber;
+
+    // Struct ( Custom types )
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public pat = Person({favoriteNumber: 5, name: "Pat"});
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    // calldata, memory => it is temporaray. storage varibles are permentant
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
